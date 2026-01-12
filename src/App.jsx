cbbca25e9f5fe9a0accb4d6db9e68d54bf67c56d
aleynakaya <devaleykaya@gmail.com>
2026-01-12 23:56:16 +0300

    style: adjust padding and margin in App component for better layout consistency

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ProfileCard from './components/ProfileCard'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-black to-white flex items-center justify-center p-4 pt-24">
+    <div className="min-h-screen bg-gradient-to-b from-black to-white flex items-center justify-center p-5 pt-25">
       <ProfileCard />
     </div>
   )

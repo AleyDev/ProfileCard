7f507909e8874f5e5e5313f0c23d362e594830f5
aleynakaya <devaleykaya@gmail.com>
2026-01-12 23:58:17 +0300

    style: remove unnecessary flex properties in App component for cleaner layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ProfileCard from './components/ProfileCard'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-black to-white flex items-center justify-center p-5 pt-25">
+    <div className="min-h-screen bg-gradient-to-b from-black to-white flex items-center justify-center p-4 pt-24">
       <ProfileCard />
     </div>
   )
